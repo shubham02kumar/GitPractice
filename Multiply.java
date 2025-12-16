b1a0c3615799050dfaec5d3c5972dee7bd5a0678
shubham02kumar <2002golu@gmail.com>
2025-12-16 22:35:29 +0530

    Second Commit

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 10;
+    int product = a * b;
+    System.out.println("The product of " + a + " and " + b + " is: " + product);
+  }
+}
