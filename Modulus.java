bb0de5f60c474a2d6b79e0ef8f265b6577d2c918
shubham02kumar <2002golu@gmail.com>
2025-12-17 09:55:38 +0530

    Third commit

---
+++ Modulus.java
@@ -0,0 +1,11 @@
+public class Modulus {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 10;
+    int remainder = b % a;
+    System.out.println("The remainder of " + b + " divided by " + a + " is: " + remainder);
+
+    System.out.println("commit test edited in Modulus.java");
+  }
+  
+}
